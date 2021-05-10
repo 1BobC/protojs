312758f1bc71b5518cc49dde8cc6abb536019010
roco <bob.cocksedge@gmail.com>
2021-05-10 15:27:37 +0100

    Complete section 15 to 125 then return to rocoWeather to complete the project

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ Constructors through Proto  ooclass construction to weather app project
 <!-- #123. Prototype Model -->
 <!-- #124. Prototypal Inheritance -->
 "Half way through 124 prototypal inheritance - take care and check code more carefully!"
+<!-- #125. Built-in Objects -->
+<!-- #126. Making a Forecast Class (weather app) - contd on rocoWeather -->
+"Complete section 15 to 125 then return to rocoWeather to complete the project"
--- jscript.js
+++ jscript.js
@@ -10,9 +10,9 @@
 //creating a code without class syntax:
 //JS uses this 'under the hood' and is how objects were created before class was introduced
 
-function User(username, email){         //User is the construction function
-    this.username= username;
-    this.email= email;
+// function User(username, email){         //User is the construction function
+//     this.username= username;
+//     this.email= email;
     // this.login= function(){
     // console.log(`${this.username}...has logged in`);       //can add a method this way 
     //but is not the best way - better to add to the prototype - why?
@@ -20,34 +20,40 @@ function User(username, email){         //User is the construction function
 }
 
     //#123. Prototype Model and whatever method we want to add we code protoype.method name
-    User.prototype.login= function(){
-        console.log(`${this.username}...has logged in`);
-        return this;            //return required for method chaining remember - undefined?
-    };
-    User.prototype.logout= function(){
-        console.log(`${this.username}...has logged out`);
-        return this;            //return required for method chaining remember - undefined?
-    };
+    // User.prototype.login= function(){
+    //     console.log(`${this.username}...has logged in`);
+    //     return this;            //return required for method chaining remember - undefined?
+    // };
+    // User.prototype.logout= function(){
+    //     console.log(`${this.username}...has logged out`);
+    //     return this;            //return required for method chaining remember - undefined?
+    // };
 
-    function Admin(username, email, title){
-        User.call(this, username, email);
-        this.title = title;
-    }
+    // function Admin(username, email, title){
+    //     User.call(this, username, email);
+    //     this.title = title;
+    // }
 
-    const userOne= new User('mario', 'mario@rococode.scot');
-    const userTwo= new User('luigi', 'luigi@rococode.scot'); 
-    const userThree= new Admin('josh', 'josh@where.aus', 'Top Car Drifter');
+    // Admin.prototype= Object.create(User.prototype);     //#124. Prototypal Inheritance
+    // Admin.prototype.deleteUser = function(){
+        //delete user
+    // };
 
-    console.log(userOne, userTwo, userThree);
+    // const userOne= new User('mario', 'mario@rococode.scot');
+    // const userTwo= new User('luigi', 'luigi@rococode.scot'); 
+    // const userThree= new Admin('josh', 'josh@where.aus', 'Top Car Drifter');
+
+    //console.log(userOne, userTwo, userThree);
     // userOne.login();
     // userOne.logout(); // and now with chaining:
-    userOne.login().logout();
+    //userOne.login().logout();
 
 //the new keyword
 //1 - it creates a new empty object {}
 //2 - it binds the value of 'this' to the new empty object
 //3 - it calls the constructor function to build the object 
 
-//#124. Prototypal Inheritance
+//#125. Built-in Objects
+//#126. Making a Forecast Class (weather app) - contd on rocoWeather
 
 
